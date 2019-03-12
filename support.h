237f78621278162d2eb22a77d740fa483d569007
X Caminhante <caminhante@i2pmail.org>
2019-03-12 20:21:57 +0000

    Completely forgot to use Git...

--- support.h
+++ support.h
@@ -1,144 +1,158 @@
+#ifndef META_SUPPORT
+#define META_SUPPORT
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
-char *output_name = NULL,
-    *token = NULL,
-    *source = NULL;
-FILE *output = NULL;
-int pos = 0,
-    pos_last_line = 0,
-    line = 1,
-    test_flag = 0,
-    ignore_whitespace = 0,
-    input_len = 0,
-    user_token=0,
-    defining_utoken = 0;
-
-void skip_whitespace (void) {
-  while ((source[pos] == '\x20' || source[pos] == '\t' ||
-  source[pos] == '\r' || source[pos] == '\n') && pos < input_len) {
+#include <inttypes.h>
+#include <stdbool.h>
+static long user_token=0,
+  token_line=0, token_column=0,
+  bytes_written=0,
+  pos=0, pos_last_line=0, line=1,
+  input_len=0;
+static FILE *output;
+static char *source=NULL, *token=NULL, *capture=NULL,
+            *input_name=NULL, *output_name=NULL;
+static bool test_flag=false, ignore_whitespace=false, mute=false;
+static char *parse_stack[10000];
+static long parse_stack_top=0;
+static char *expecting=NULL, *unexpected=NULL, *reason=NULL;
+static void start_line (void) {
+  pos_last_line=pos+1;
+  line++;
+}
+static char* concatstr (char *txt, const char *concat) {
+  int txtlen = strlen(txt),
+      conclen = strlen(concat);
+  char *newbuf = realloc(txt,txtlen+conclen+1);
+  strcpy(newbuf+txtlen,concat);
+  return newbuf;
+}
+static void enter_parse_rule (char *parse_rule) {
+  parse_stack[parse_stack_top++]=parse_rule;
+}
+static void exit_parse_rule () {
+  parse_stack_top--;
+}
+static bool is_space (char c) {
+  return (c==' ' || c=='\t' || c=='\r' || c=='\n');
+}
+static void skip_whitespace (void) {
+  test_flag=false;
+  while (is_space(source[pos]) && pos<input_len) {
+    test_flag=true;
     if (source[pos] == '\n') {
-      pos_last_line = pos;
-      line++;
+      start_line();
     }
     pos++;
   }
 }
-
-void make_token (int start_pos) {
-  if (defining_utoken) return;
-  int length = pos - start_pos;
-  free(token);
+static void make_token (int start_pos) {
+  token_line = line;
+  token_column = start_pos-pos_last_line+1;
+  int length = pos-start_pos;
+  if (token) free(token);
   token = malloc(length + 1);
   token[length] = '\0';
   memcpy(token, &source[start_pos], length);
 }
-
+void capture_to_token () {
+  if (token) free(token);
+  token=capture;
+  capture=NULL;
+}
+static void emit (const char *str) {
+  if (mute) return;
+  if (!capture) {
+    fprintf(output, "%s", str); bytes_written += strlen(str);
+  } else {
+    capture = concatstr(capture,str);
+  }
+}
+static void emit_char (char c) {
+  if (mute) return;
+  if (!capture) {
+    fputc(c, output); bytes_written++;
+  } else {
+    capture = concatstr(capture,(char[2]){c,'\0'});
+  }
+}
 void emit_token (int quote) {
-  int i;
-  if (token[0] == '\'') {
-    if (quote) fprintf(output, "\"");
-    for (i = 1; token[i+1] != '\0'; i++) {
+  if (mute || !token) return;
+  if (!quote && strlen(token) == 1) {
+    emit_char(token[0]); return;
+  }
+  int i=1;
+  if (token[0] == '\'' || token[0] == '"') {
+    if (quote) emit_char('"');
+    for (; token[i+1] != '\0'; i++) {
       switch (token[i]) {
-      case '\"':
+      case '"':
         if (!quote) goto def_case;
-        fprintf(output, "\\\"");
-        break;
+        emit("\\\"");
+      break;
       case '\n':
         if (!quote) goto def_case;
-        fprintf(output, "\\n");
-        break;
+        emit("\\n");
+      break;
+      case '\r':
+        if (!quote) goto def_case;
+        emit("\\r");
+      break;
+      case '\\':
+        if (!quote) {
+          i++;
+          switch(token[i]) {
+          case 'n': emit_char('\n'); break;
+          case 'r': emit_char('\r'); break;
+          default: goto def_case;
+          }
+          break;
+        }
       default:
         def_case:
-        fprintf(output, "%c", token[i]);
-        break;
+        emit_char(token[i]);
       }
     }
-    if (quote) fprintf(output, "\"");
+    if (quote) emit_char('"');
     return;
   }
-  fprintf(output, "%s", token);
+  emit(token);
 }
-
-void emit (const char *str) {
-  fprintf(output, "%s", str);
-}
-
 void read_literal (const char *literal) {
   int entry_pos;
   int i;
   if (!ignore_whitespace) skip_whitespace();
-  entry_pos = pos;
-  i = 0;
-  while (pos < input_len && literal[i] != '\0' && source[pos] == literal[i]) {
+  entry_pos=pos;
+  i=0;
+  while (pos<input_len && literal[i] != '\0' && source[pos] == literal[i]) {
     pos++;
     i++;
   }
   if (literal[i] == '\0') {
-    test_flag = 1;
+    test_flag=true;
     make_token(entry_pos);
   } else {
-    pos = entry_pos;
-    test_flag = 0;
+    pos=entry_pos;
+    test_flag=false;
   }
 }
-
-void read_any_between (char first, char last) {
-  if (pos < input_len && source[pos] >= first && source[pos] <= last) {
-    pos++;
-    test_flag=1;
-    make_token(pos-1);
-    return;
-  }
-  test_flag=0;
-}
-
-void read_any_but (char first, char last) {
-  if (pos < input_len && (source[pos] < first || source[pos] > last)) {
-    pos++;
-    test_flag=1;
-    make_token(pos-1);
-    return;
-  }
-  test_flag=0;
-}
-
-void read_char_eq (char which) {
-  if (pos < input_len && source[pos] == which) {
-    pos++;
-    test_flag=1;
-    make_token(pos-1);
-    return;
-  }
-  test_flag=0;
-}
-
-void read_char_neq (char which) {
-  if (pos < input_len && source[pos] != which) {
-    pos++;
-    test_flag=1;
-    make_token(pos-1);
-    return;
-  }
-  test_flag=0;
-}
-
-void read_id (void) {
+void read_id () {
   int entry_pos;
   if (!ignore_whitespace) skip_whitespace();
-  entry_pos = pos;
-  if (pos < input_len &&
+  entry_pos=pos;
+  if (pos<input_len &&
   (('A' <= source[pos] && source[pos] <= 'Z') ||
   ('a' <= source[pos] && source[pos] <= 'z'))) {
     pos++;
-    test_flag = 1;
+    test_flag=true;
   } else {
-    test_flag = 0;
+    test_flag=false;
     return;
   }
-  while (pos < input_len &&
+  while (pos<input_len &&
   (('A' <= source[pos] && source[pos] <= 'Z') ||
   ('a' <= source[pos] && source[pos] <= 'z') ||
   ('0' <= source[pos] && source[pos] <= '9') || source[pos] == '_')) {
@@ -146,123 +160,274 @@ void read_id (void) {
   }
   make_token(entry_pos);
 }
-
-void read_number (void) {
+void read_string () {
   int entry_pos;
   if (!ignore_whitespace) skip_whitespace();
-  entry_pos = pos;
-  if (pos < input_len && source[pos] == '-') {
-    pos++;
-  }
-  if (pos < input_len && '0' <= source[pos] && source[pos] <= '9') {
-    pos++;
-    test_flag = 1;
-  } else {
-    test_flag = 0;
-    return;
-  }
-  while (pos < input_len && '0' <= source[pos] && source[pos] <= '9') {
-    pos++;
-  }
-  make_token(entry_pos);
-}
-
-void read_string (void) {
-  int entry_pos;
-  if (!ignore_whitespace) skip_whitespace();
-  entry_pos = pos;
-  if (pos < input_len && source[pos] == '\'') {
+  entry_pos=pos;
+  if (pos<input_len && ( source[pos] == '\'' || source[pos] == '"')) {
     pos++;
   } else {
-    test_flag = 0;
+    test_flag=false;
     return;
   }
-  while (pos < input_len && source[pos] != '\'') {
+  while (pos<input_len && source[pos] != source[entry_pos]) {
     switch (source[pos]) {
     case '\n':
-      pos_last_line = pos;
-      line++;
-      break;
+      start_line();
+    break;
     case '\\':
       pos++;
     }
     pos++;
   }
-  if (source[pos] == '\'') {
+  if (source[pos] == source[entry_pos]) {
     pos++;
-    test_flag = 1;
+    test_flag=true;
     make_token(entry_pos);
-  } else if (pos < input_len) {
-    pos = entry_pos;
-    test_flag = 0;
+  } else {
+    pos=entry_pos;
+    test_flag=false;
   }
 }
-
-void error_if_false (void) {
-  if (!test_flag) {
-    fprintf(stderr, "error in line:column %i:%i at token %s\n", line, pos-pos_last_line, token);
+void read_number () {
+  int entry_pos;
+  if (!ignore_whitespace) skip_whitespace();
+  entry_pos=pos;
+  if (pos<input_len && source[pos] == '-') {
+    pos++;
+  }
+  if (pos<input_len && '0' <= source[pos] && source[pos] <= '9') {
+    pos++;
+    test_flag=true;
+  } else {
+    test_flag=false;
+    return;
+  }
+  while (pos<input_len && '0' <= source[pos] && source[pos] <= '9') {
+    pos++;
+  }
+  make_token(entry_pos);
+}
+void read_any_between (char first, char last) {
+  if (pos<input_len && source[pos] >= first && source[pos] <= last) {
+    if (source[pos] == '\n') {
+      start_line();
+    }
+    pos++;
+    test_flag=true;
+    return;
+  }
+  test_flag=false;
+}
+void read_any_but (char first, char last) {
+  if (pos<input_len && (source[pos]<first || source[pos] > last)) {
+    if (source[pos] == '\n') {
+      start_line();
+    }
+    pos++;
+    test_flag=true;
+    return;
+  }
+  test_flag=false;
+}
+void read_char_eq (char which) {
+  if (pos<input_len && source[pos] == which) {
+    if (which == '\n') {
+      start_line();
+    }
+    pos++;
+    test_flag=true;
+    return;
+  }
+  test_flag=false;
+}
+void read_char_neq (char which) {
+  if (pos<input_len && source[pos] != which) {
+    if (which == '\n') {
+      start_line();
+    }
+    pos++;
+    test_flag=true;
+    return;
+  }
+  test_flag=false;
+}
+static void initialize_parser () {
+  if (output) {
     fclose(output);
-    remove(output_name);
-    free(source);
+    output=NULL;
+  }
+  if (token) {
     free(token);
+    token=NULL;
+  }
+  if (output_name) {
+    if ((char*)output_name != (char*)"stdout") free(output_name);
+    output_name=NULL;
+  }
+  if (input_name) {
+    if ((char*)input_name != (char*)"stdin") free(input_name);
+    input_name=NULL;
+  }
+  if (source) {
+    free(source);
+    source=NULL;
+  }
+  if (capture) {
+    free(capture);
+    capture=NULL;
+  }
+  parse_stack_top=0;
+  pos=0;
+  pos_last_line=0;
+  line=1;
+  test_flag=false;
+  ignore_whitespace=false;
+  mute=false;
+  input_len=0;
+  user_token=0;
+  token_line=0;
+  token_column=0;
+  bytes_written=0;
+  expecting=NULL;
+  unexpected=NULL;
+}
+static void finalize_parser () {
+  fclose(output);
+  if (output_name) remove(output_name);
+  exit(1);
+}
+static void status_line () {
+  if (input_name) {
+    fprintf(stderr, "%s:", input_name);
+  }
+  fprintf(stderr, "%li:%li: Rule %s",
+          line, pos-pos_last_line+1,
+          parse_stack[parse_stack_top-1]);
+  if (token && token[0] != '\0') {
+    fprintf(stderr,"\nLast token was '%s', starting at %li:%li",
+            token,token_line,token_column);
+  }
+}
+void warning (const char *msg) {
+  status_line();
+  fprintf(stderr,"\nWarning: %s\n", msg);
+}
+void error (const char *msg) {
+  status_line();
+  fprintf(stderr,"\nError: %s\n", msg);
+  finalize_parser();
+}
+void error_if_false () {
+  if (!test_flag) {
+    status_line();
+    if (reason) {
+        fputc('\n', stderr);
+        fprintf(stderr,reason);
+    } else {
+        fprintf(stderr,"\nParsing error");
+    }
+    if (expecting) {
+        fprintf(stderr,", expecting %s",expecting);
+    }
+    if (unexpected) {
+        fprintf(stderr,", unexpected %s",unexpected);
+    }
+    fputc('\n', stderr);
+    finalize_parser();
+  }
+}
+int first_into_second(int argc, char *argv[], void(*func)()) {
+  FILE *input;
+  if (argc > 3) {
+    fprintf(stderr, "Usage: meta [<input>] [<output>]\n");
     exit(1);
   }
+  if (argc == 1) {
+    input=stdin;
+    input_name="stdin";
+    source=malloc(4194304+1);
+    input_len=read(0,source,4194304);
+  } else {
+    input=fopen(argv[1], "r");
+    if (input == NULL) {
+      fprintf(stderr, "Invalid input file\n");
+      exit(1);
+    }
+    input_name = argv[1];
+    fseek(input, 0, SEEK_END);
+    input_len = ftell(input);
+    fseek(input, 0, SEEK_SET);
+    source = malloc(input_len + 1);
+    fread(source, 1, input_len, input);
+  }
+  if (argc<3) {
+    output=stdout;
+    output_name="stdout";
+  } else {
+    output_name=argv[2];
+    output=fopen(output_name, "w");
+    if (output == NULL) {
+      fprintf(stderr, "Invalid output file\n");
+      exit(1);
+    }
+  }
+  source[input_len]='\0';
+
+  func();
+  fprintf(stderr, "%li %li %li\n", input_len, pos, bytes_written);
+  if (!bytes_written) {
+    remove(output_name);
+  }
+  return 0;
+}
+void multiple_files (int argc, char *argv[], char* extension, void(*func)()) {
+  for (int a=1; a<argc; a++) {
+    fprintf(stderr,"Processing file #%d...\n",a);
+    initialize_parser();
+
+    int input_name_len = strlen(argv[a]);
+    output_name = malloc(input_name_len + strlen(extension) + 2);
+    input_name = malloc(input_name_len);
+    strcpy(output_name,argv[a]);
+    strcpy(input_name,argv[a]);
+    strcat(&output_name[input_name_len],extension);
+
+    FILE* input=fopen(argv[a], "r");
+    if (input == NULL) {
+      fprintf(stderr, "Input file '%s' invalid\n",argv[a]);
+      exit(1);
+    }
+    output=fopen(output_name, "w");
+    if (output == NULL) {
+      fprintf(stderr, "Can't write to output file '%s'\n",output_name);
+      exit(1);
+    }
+
+    fseek(input, 0, SEEK_END);
+    input_len=ftell(input);
+    fseek(input, 0, SEEK_SET);
+    source=malloc(input_len + 1);
+    fread(source, 1, input_len, input);
+    source[input_len]='\0';
+
+    func();
+    fprintf(stderr, "%li %li %li\n", input_len, pos, bytes_written);
+    if (!bytes_written) {
+      remove(output_name);
+    }
+  }
+}
+int test (int source_len, void(*func)()) {
+  source=malloc(source_len+1);
+  int chars=read(0,source,source_len);
+  input_len=chars;
+  source[chars]='\0';
+  output=stdout;
+  input_name="stdin";
+  output_name="stdout";
+  func();
+  return 0;
 }
 
-void meta_program (void);
-void meta_exp1 (void);
-
-//~ int main (int argc, char *argv[]) {
-  //~ source = malloc(65535+1);
-  //~ int chars = read(0,source,65535);
-  //~ source[chars] = '\0';
-  //~ input_len = chars;
-  //~ output = stdout;
-  //~ token = malloc(1);
-  //~ token[0] = '\0';
-//~ }
-
-//~ int main(int argc, char *argv[]) {
-  //~ FILE *input;
-  //~ int length;
-//~
-  //~ if (argc != 3) {
-    //~ fprintf(stderr, "usage: meta <input> <output>\n");
-    //~ exit(1);
-  //~ }
-//~
-  //~ // open input and output
-  //~ input = fopen(argv[1], "r");
-  //~ if (input == NULL) {
-    //~ fprintf(stderr, "invalid input file\n");
-    //~ exit(1);
-  //~ }
-  //~ output_name = argv[2];
-  //~ output = fopen(output_name, "w");
-  //~ if (output == NULL) {
-    //~ fprintf(stderr, "invalid output file\n");
-    //~ exit(1);
-  //~ }
-  //~ // read entire input into source
-  //~ fseek(input, 0, SEEK_END);
-  //~ length = (int)ftell(input);
-  //~ fseek(input, 0, SEEK_SET);
-  //~ source = malloc(length + 1);
-  //~ fread(source, 1, length, input);
-  //~ source[length] = '\0';
-  //~ fclose(input);
-//~
-  //~ // initially we have empty token; token is never NULL
-  //~ token = malloc(1);
-  //~ token[0] = '\0';
-//~
-  //~ // run meta
-  //~ meta_program();
-//~
-  //~ fprintf(stderr, "%d %d\n", pos, strlen(source));
-//~
-  //~ fclose(output);
-  //~ free(source);
-  //~ free(token);
-  //~ return 0;
-//~ }
+#endif
